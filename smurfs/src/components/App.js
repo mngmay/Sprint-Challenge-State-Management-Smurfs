ec69421517d83a117d4a3bff8f95bde142ba3109
May Ng <mngmay@gmail.com>
2019-08-16 08:37:35 -0700

    Created getData in actions. Successfully fetching data from API

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,33 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import React, { Component, useEffect } from "react";
+import { connect } from "react-redux";
+import { getData } from "../actions";
 
-export default App;
+import "./App.css";
+const App = props => {
+  useEffect(() => {
+    props.getData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    name: state.name,
+    age: state.age,
+    height: state.height,
+    id: state.id
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getData }
+)(App);
