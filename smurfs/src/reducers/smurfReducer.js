73102bc77c267393a7a6551c79ecec2edbea1ced
May Ng <mngmay@gmail.com>
2019-08-16 08:22:42 -0700

    Created folders to organize actions and reducers. Set up Redux store

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  name: "",
+  age: "",
+  height: "",
+  id: ""
+};
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
